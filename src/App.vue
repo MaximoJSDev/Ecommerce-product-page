487af9874f4625dddfdcb6919d340aa7d147aeca
Maximo-4ever <maximilianocm06@gmail.com>
2022-03-27 19:52:06 -0600

    Ajusta el diseño de escritorio

--- src/App.vue
+++ src/App.vue
@@ -20,19 +20,93 @@ html {
   box-sizing: inherit;
 }
 :root {
-/* ### Primary */
---Orange: hsl(26, 100%, 55%);
---Pale-orange: hsl(25, 100%, 94%);
+  /* ### Primary */
+  --Orange: hsl(26, 100%, 55%);
+  --Pale-orange: hsl(25, 100%, 94%);
 
-/* ### Neutral */
---Very-vark-blue: hsl(220, 13%, 13%);
---Dark-grayish-blue: hsl(219, 9%, 45%);
---Grayish-blue: hsl(220, 14%, 75%);
---Light-grayish-blue: hsl(223, 64%, 98%);
---White: hsl(0, 0%, 100%);
---Black: hsl(0, 0%, 0%); /*(with 75% opacity for lightbox background)*/
+  /* ### Neutral */
+  --Very-vark-blue: hsl(220, 13%, 13%);
+  --Dark-grayish-blue: hsl(219, 9%, 45%);
+  --Grayish-blue: hsl(220, 14%, 75%);
+  --Light-grayish-blue: hsl(223, 64%, 98%);
+  --White: hsl(0, 0%, 100%);
+  --Black: hsl(0, 0%, 0%); /*(with 75% opacity for lightbox background)*/
 }
 body {
   font-family: 'Kumbh Sans', sans-serif;
 }
+@media (min-width: 1340px) {
+  .header {
+    padding: 35px 0 40px !important;
+    border-bottom: 1px solid var(--Grayish-blue);
+  }
+  .header__iconMenu {
+    display: none;
+  }
+  .header__menu__linksContainer {
+    display: block !important;
+  }
+  .header__profile__avatar {
+    height: 57px !important;
+  }
+  .product {
+    max-width: 1200px;
+    flex-direction: row;
+    margin-top: 5rem;
+  }
+  .product__images {
+    flex-direction: column;
+    row-gap: 25px;
+  }
+  .product__slider {
+    height: 520px;
+    border-radius: 20px;
+  }
+  .product__figure {
+    display: flex;
+    gap: 20px;
+    justify-content: space-between;
+  }
+  .product__figure img {
+    height: 110px;
+    border-radius: 10px;
+  }
+  .product__figure img:hover {
+    opacity: .65;
+  }
+  .imgPreview--active {
+    outline: 3px solid var(--Orange);
+    opacity: .36;
+  }
+  .product__info {
+    justify-content: center;
+    padding-left: 9rem;
+    margin-bottom: 0;
+  }
+  .product__info__company {
+    font-size: 17px;
+  }
+  .product__info__title {
+    font-size: 46px;
+    margin-bottom: 40px;
+  }
+  .product__info__priceContainer {
+    flex-direction: column;
+    align-items: flex-start;
+    row-gap: 10px;
+  }
+  .btnContainer {
+    flex-direction: row;
+    justify-content: unset;
+    align-items: center;
+    column-gap: 25px;
+  }
+  .btn-count {
+    width: 35%;
+  }
+  .btn-orange {
+    width: 77%;
+  }
+}
+
 </style>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -7,6 +7,15 @@
     <div class="header__menu">
       <img class="header__iconMenu" src="../assets/icon-menu.svg" alt="icon menu">
       <img class="header__logo" src="../assets/logo.svg" alt="logo">
+    <div class="header__menu__linksContainer">
+      <ul class="links">
+        <li>Collections</li>
+        <li>Men</li>
+        <li>Women</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </div>
     </div>
     <div class="header__profile">
       <img class="header__profile__cart" src="../assets/icon-cart.svg" alt="icon cart">
@@ -17,12 +26,14 @@
 
 <style scoped>
 .header {
+  max-width: 1280px;
   display: flex;
   align-items: center;
   justify-content: space-between;
   padding: 26px 30px;
+  margin: auto;
 }
-.header div {
+.header div:not(.header__menu__linksContainer) {
   display: flex;
   align-items: center;
   justify-content: space-between;
@@ -41,5 +52,20 @@
 }
 .header__profile__avatar {
   height: 38px;
+}
+.header__menu__linksContainer {
+  display: none;
+}
+.header__menu__linksContainer ul {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  list-style: none;
+  column-gap: 30px;
+  font-size: 18px;
+  margin-left: 35px;
+}
+.header__menu__linksContainer li {
+  cursor: pointer;
 }
 </style>
