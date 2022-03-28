5e2278d7fe1b2617dab5df8261850fa1aaa56a70
Maximo-4ever <maximilianocm06@gmail.com>
2022-03-27 19:26:15 -0600

    Crea el componente ProductImage

---
+++ src/components/ProductImage.vue
@@ -0,0 +1,27 @@
+<script setup>
+defineProps({
+  id: String,
+  isShowImg: {type: Boolean, default: false},
+  isPreviewImg: {type: Boolean, default: false},
+});
+const getImgUrl = (name, isPreviewImg) => {
+  const src = isPreviewImg 
+    ? new URL(`../assets/image-product-${name}-thumbnail.jpg`, import.meta.url).href
+    : new URL(`../assets/image-product-${name}.jpg`, import.meta.url).href
+  return src;
+};
+const getImgClass = (isPreviewImg, isShowImg) => {
+  const classImg = isPreviewImg
+    ? ''
+    : `product__slider__image ${isShowImg && 'imageSliderShow'}`
+  return classImg;
+};
+</script>
+<template>
+  <img
+    :class="getImgClass(isPreviewImg, isShowImg)"
+    :src="getImgUrl(id, isPreviewImg)"
+    :alt="`image-product-${id}`"
+    :data-id="id"
+  >
+</template>
